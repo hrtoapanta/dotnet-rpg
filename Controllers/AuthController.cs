9421bae902e4f6776a980746bc79d892c8a33d75
hrtoapanta <hrtoapanta@gmail.com>
2023-06-06 16:57:36 +0200

    Add autentication repo, controller and register User

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_rpg.Dtos.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    { 
+        private readonly IAuthRepository _autRepo;
+        public AuthController(IAuthRepository autRepo)
+        {
+            _autRepo = autRepo;
+            
+        }
+        [HttpPost("Register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDto request)
+        {
+            var response = await _autRepo.Register(
+                new User{UserName=request.UserName}, request.Password
+            );
+            if(!response.Success){
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+        
+    }
+}
---
+++ Data/AuthRepository.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _context;
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+            
+        }
+        public Task<ServiceResponse<string>> Login(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            var response = new ServiceResponse<int>();
+            if(await UserExists(user.UserName)){
+                response.Success=false;
+                response.Message ="user alreadyu exists";
+                return response;
+            }
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            
+            
+            response.Data = user.Id;
+            return response;
+        }
+
+        public async Task<bool> UserExists(string userName)
+        {
+            if(await _context.Users.AnyAsync(u=>u.UserName.ToLower()==userName.ToLower())){
+                return true;
+            }
+            return false;
+        }
+
+        private void CreatePasswordHash (string password, out byte[] passwordHash, out byte[] passwordSalt )
+        {
+
+            using(var hmac = new System.Security.Cryptography.HMACSHA512()){
+
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+
+        }
+    }
+}
