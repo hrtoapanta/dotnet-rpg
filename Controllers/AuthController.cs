770930f8a99ffb403266d642caac9c5052b78e23
hrtoapanta <hrtoapanta@gmail.com>
2023-06-12 09:58:10 +0200

    add JwtBearerToken

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using dotnet_rpg.Dtos.User;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_rpg.Controllers
 {
+    
     [ApiController]
     [Route("[controller]")]
     public class AuthController : ControllerBase
@@ -23,6 +25,15 @@ namespace dotnet_rpg.Controllers
             var response = await _autRepo.Register(
                 new User{UserName=request.UserName}, request.Password
             );
+            if(!response.Success){
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+          [HttpPost("Login")]
+        public async Task<ActionResult<ServiceResponse<int>>> Login(UserLoginDto request)
+        {
+            var response = await _autRepo.Login(request.UserName, request.Password);
             if(!response.Success){
                 return BadRequest(response);
             }
